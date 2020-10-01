53f9cdc245d9ae17b72d9fd250f6599cf413e7e0
mearghya <72182313+mearghya@users.noreply.github.com>
2020-10-01 23:15:05 +0530

    Add files via upload

---
+++ DICTIONARY.py
@@ -0,0 +1,12 @@
+print("DICTIONARY")
+DICTIONARY={"ROSE":"A kind of flower" ,"CYCLE":"A kind of vehicle" ,"BEE":"A kind of insect" ,"COTTAGE":"A type of house"}
+# print(DICTIONARY)
+print("ENTER YOUR WORD-\n")
+W=input()
+WORD=W.upper()
+MEANING=DICTIONARY.get(WORD)
+print("THE MEANING IS-\n")
+print(MEANING)
+print("Is it okay?")
+input()
+print("THANK YOU")
